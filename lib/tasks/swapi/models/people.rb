127b8486ff6c0ab45d7632200b1e866951b916a4
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-05 20:23:11 -0300

    Alteração do model people para adicionar a importação de starships, species e homeworld.

--- lib/tasks/swapi/models/people.rb
+++ lib/tasks/swapi/models/people.rb
@@ -1,34 +1,57 @@
 class PeopleModel  
     
     def findAll(page=1)
+        peoples = Swapi.findAll('people')
     
         lista_peoples = Array.new
-
-        peoples = JSON.parse(Swapi.get_all "people?page=#{page}")
-
-        peoples["results"].each{ |people| 
+        peoples.each{ |people| 
 
             people_entity = People.new()
-            
+            people_entity[:id]          = Swapi.get_id_from_url(people["url"])
             people_entity[:name]        = people["name"]
             people_entity[:birth_year]  = people["birth_year"]
             people_entity[:eye_color]   = people["eye_color"]
             people_entity[:hair_color]  = people["hair_color"]
             people_entity[:gender]      = people["gender"]
             people_entity[:height]      = people["height"]
-            people_entity[:homeworld]   = people["homeworld"]
+            people_entity[:homeworld]   = Swapi.get_id_from_url(people["homeworld"])
             people_entity[:mass]        = people["mass"]
             people_entity[:skin_color]  = people["skin_color"]
 
+            next if not people_entity.save
+
+            
+            people["species"].each{ |specie_url|  
+                
+                specie_id = Swapi.get_id_from_url(specie_url)
+                
+                people_specie = PeopleSpecies.new()
+                people_specie[:people_id] = people_entity[:id]
+                people_specie[:specie_id] = specie_id
+                people_specie.save
+
+            }
+
+            people["starships"].each{ |starship_url|  
+                
+                starship_id = Swapi.get_id_from_url(starship_url)
+                
+                begin
+                    starship = Starship.find(starship_id)
+                rescue => e
+                    next
+                end
+                
+
+                people_starship = PeopleStarships.new()
+                people_starship[:people_id] = people_entity[:id]
+                people_starship[:starships_id] = starship_id
+                people_starship.save
+
+            }
+
             lista_peoples << people_entity
         }
-
-        if not peoples["next"].nil?
-            
-            next_page = page + 1
-
-            lista_peoples.concat self.findAll(next_page)
-        end
 
         return lista_peoples
 
