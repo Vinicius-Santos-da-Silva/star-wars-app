bb80912e3d23c65136bc845b83226670c046fb44
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-05 20:25:28 -0300

    Inclusão de método para importar todas as páginas atráves da api swapi.

--- lib/tasks/swapi/swapi.rb
+++ lib/tasks/swapi/swapi.rb
@@ -2,40 +2,41 @@ require 'open-uri'
 
 module Swapi
   class << self
-    BASE_URL = 'http://swapi.dev/api'
-    PLANETS = 'planets'
-    PEOPLE = 'people'
-    STARSHIPS = 'starships'
-    VEHICLES = 'vehicles'
-    SPECIES = 'species'
-    FILMS = 'films'
+    
+    BASE_URL = 'http://swapi.dev/api/'
+
+    def findAll(entity = 'people', page=1)
+    
+      list = Array.new
+
+      response = JSON.parse(Swapi.get_all "#{entity}?page=#{page}")
+
+      response["results"].each{ |data| 
+          
+          list << data
+      }
+
+      if not response["next"].nil?
+          
+          next_page = page + 1
+
+          list.concat self.findAll(entity , next_page)
+      end
+
+      return list
+    end
+
+    def get_id_from_url(obj)
+      
+      if obj.nil?
+        return 0;  
+      end
+
+      return obj.gsub(/[^\d]/, '')
+    end
 
     def get_all(type)
       get type
-    end
-
-    def get_planet(planet_id)
-      get PLANETS, planet_id
-    end
-
-    def get_person(people_id)
-      get PEOPLE, people_id
-    end
-
-    def get_starship(starship_id)
-      get STARSHIPS, starship_id
-    end
-
-    def get_vehicle(vehicle_id)
-      get VEHICLES, vehicle_id
-    end
-
-    def get_species(species_id)
-      get SPECIES, species_id
-    end
-
-    def get_film(film_id)
-      get FILMS, film_id
     end
 
     def request(url)
@@ -56,6 +57,6 @@ module Swapi
       response = open("#{BASE_URL}/#{uri}", "User-Agent" => "swapi-ruby").read
     end
 
-    
+
   end
 end
