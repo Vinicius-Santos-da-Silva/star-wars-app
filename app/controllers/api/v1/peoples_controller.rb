7d56cd1e7be77ef6f0290d6af58a42645f84c554
Vinicius Santos <vinicius.silva@gabster.com.br>
2021-09-05 20:26:55 -0300

    Inclusão dos dados relacionados com a entidade people(planet , species e staships).

--- app/controllers/api/v1/peoples_controller.rb
+++ app/controllers/api/v1/peoples_controller.rb
@@ -1,11 +1,28 @@
 class Api::V1::PeoplesController < ApplicationController
     
     def index
-        render json: People.limit(params[:limit]).offset(params[:offset]), status: 200
+        peoples = People.limit(params[:limit]).offset(params[:offset])
+        render json: peoples, status: 200
     end
 
     def show
-        people = People.find(params[:id])
+        people = People.find(params[:id]).as_json
+        
+        people["starships"] = Array.new
+        people["species"] = Array.new
+
+        starships = PeopleStarships.where(:people_id => params[:id])
+        species   = PeopleSpecies.where(:people_id => params[:id])
+        
+        starships.each{ |starship|  
+            people["starships"] << Starship.find(starship[:starships_id])
+        }
+
+        species.each{ |specie|  
+            people["species"] << Species.find(specie[:specie_id])
+        }
+        
+        people["homeworld"] = Planet.find(people["homeworld"])
         
         render json: people , status: 200
     end
